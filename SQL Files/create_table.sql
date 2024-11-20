2b06b8a88b6b82217dd9291533429969bb10e40c
nyte-panda <mthuthimal@gmail.com>
2024-11-20 15:04:58 -0500

    Add files via upload

---
+++ SQL Files/create_table.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE SportsComplex(
+SportComplex_PK INT NOT NULL,
+CL VARCHAR(50),
+CCOI VARCHAR(50),
+CTOA VARCHAR(50),
+primary key (SportsComplex_PK)
+);
+
+CREATE TABLE EVENT(
+Event INT NOT NULL,
+ED VARCHAR(50),
+ENOfP VARCHAR(50),
+ENOfO VARCHAR(50),
+EPDate VARCHAR(50),
+primary key (Event),
+);
+
