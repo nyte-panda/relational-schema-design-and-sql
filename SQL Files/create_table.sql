a5ecacb63c4aeb000ca88d155ecba452a5a5dee5
Winnielepuh <114607910+Winnielepuh@users.noreply.github.com>
2024-11-20 22:46:58 -0500

    Update create_table.sql

--- SQL Files/create_table.sql
+++ SQL Files/create_table.sql
@@ -24,14 +24,21 @@ primary key (Event_PK),
 foreign key (SportsComplex_PK)
 );
 
--- There should be more attributes but I don't know which ones, K: I added the relationships for the 1:M for onesport complex and multisportcomplex
+-- There should be more attributes but I don't know which ones, 
+--I added the relationships for the 1:M for onesport complex, multisportcomplex and area multivalue 
 CREATE TABLE MultiSportComplex(
 MultiSportComplex_PK INT NOT NULL, 
 ComplexType_PK INT NOT NULL,
 primary key (MultiSportComplex_PK),
 foreign key (ComplexType_PK) References ComplexType (ComplexType_PK)
 );
-
+CREATE TABLE AREA(
+AREA_PK NOT NULL,
+MultiSportComplex_PK INT NOT NULL, 
+LI VARCHAR(50), 
+primary key (MultiSportComplex_PK, AREA_PK),
+foreign key (MultiSportComplex_PK) References MultiSportComplex (MultiSportComplex_PK)
+);
 
 CREATE TABLE OneSportComplex(
 OneSportComplex_PK INT NOT NULL,
