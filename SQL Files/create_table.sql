46b3e17765b4979542b5acb214ba5a59ec716ef8
nyte-panda <mthuthimal@gmail.com>
2024-11-27 20:19:33 -0500

    Update create_table.sql

--- SQL Files/create_table.sql
+++ SQL Files/create_table.sql
@@ -1,84 +1,47 @@
 
 CREATE TABLE SportsComplex(
-SportsComplex_PK INT NOT NULL,
-CL VARCHAR(50),
-CCOI VARCHAR(50),
-CTOA VARCHAR(50),
-primary key (SportsComplex_PK)
+SportsComplex_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CL VARCHAR(50) NOT NULL,
+CCOI VARCHAR(50) NOT NULL,
+CTOA VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE ComplexType (
-ComplexType_PK INT NOT NULL,
-Budget VARCHAR(50),
-NOfF VARCHAR(50),
-PRIMARY KEY (ComplexType_PK)
-);
-
-CREATE TABLE EVENT(
-Event_PK INT NOT NULL,
-SportsComplex_PK INT NOT NULL,
+CREATE TABLE Event(
+Event_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+EPDate VARCHAR(50),
 ED VARCHAR(50),
 ENOfP VARCHAR(50),
-ENOfO VARCHAR(50),
-EPDate VARCHAR(50),
-primary key (Event_PK),
-foreign key (SportsComplex_PK) References SportsComplex (SportsComplex_PK)
+ENOfO VARCHAR(50)
 );
 
-CREATE TABLE MultiSportComplex(
-MultiSportComplex_PK INT NOT NULL, 
-ComplexType_PK INT NOT NULL,
-primary key (MultiSportComplex_PK),
-foreign key (ComplexType_PK) References ComplexType (ComplexType_PK)
-);
-CREATE TABLE Area(
-MultiSportComplex_PK INT NOT NULL, 
-Area_PK INT NOT NULL,        
-LI VARCHAR(50), 
-primary key (MultiSportComplex_PK, AREA_PK),
-foreign key (MultiSportComplex_PK) References MultiSportComplex (MultiSportComplex_PK)
+CREATE TABLE Holds(
+Holds_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+FOREIGN KEY (Holds_PK) REFERENCES Holds(Holds_PK)
 );
 
-CREATE TABLE OneSportComplex(
-OneSportComplex_PK INT NOT NULL,
-ComplexType_PK INT NOT NULL,
-primary key (OneSportComplex_PK),
-foreign key (ComplexType_PK) References ComplexType (ComplexType_PK)
+CREATE TABLE Official(
+Offical_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-CREATE TABLE holds (
-SportsComplex_PK INT NOT NULL,
-Event_PK INT NOT NULL,
-primary key (SportsComplex_PK, Event_PK), 
-foreign key (SportsComplex_PK) References SportsComplex (SportsComplex_PK),
-foreign key (Event_PK) References Event (Event_PK)
+CREATE TABLE Equipment(
+Equipment_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-CREATE TABLE Official (
-Official_PK INT NOT NULL,
-primary key (Official_PK)
+CREATE TABLE Officiate(
+Officiate_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-CREATE TABLE equipment (
-Equipment_PK INT NOT NULL,
-primary key (Equipment_PK)
+CREATE TABLE Uses(
+Uses_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-Create TABLE Officiate (
-Event_PK INT NOT NULL,
-Official_PK INT NOT NULL,
-primary key (Event_PK, Official_PK), 
-foreign key (Event_PK) References Event (Event_PK),
-foreign key (Official_PK) References Official (Official_PK)
-);
+CREATE TABLE ComplexType(
+Complex_PK INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+NOfF VARCHAR(50),
+Budget VARCHAR(50)
+); -- Need to add more variables -- 
 
-Create TABLE Uses (
-Event_PK INT NOT NULL,
-Equipment_PK INT NOT NULL,
-primary key (Event_PK, Equipment_PK), 
-foreign key (Event_PK) References Event (Event_PK),
-foreign key (Equipment_PK) References Equipment (Equipment_PK)
-);
 
+-- There should be 3 more tables (OneSportComplex, MultiSportComplex, Area) --
 
 
