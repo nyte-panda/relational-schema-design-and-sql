c1557c4819abb50f9f94c1b4796a8f88d02bc220
nyte-panda <mthuthimal@gmail.com>
2024-11-29 12:43:41 -0500

    Update insert_variables.sql

--- SQL Files/insert_variables.sql
+++ SQL Files/insert_variables.sql
@@ -2,3 +2,9 @@ INSERT INTO SportsComplex (CL, CCOI, CTOA)
 VALUES
 ('MidTown Sports Complex', 'Jane Doe', 'Football'),
 ('Uptown Training Center', 'Jane Smith', 'Basketball');
+
+INSERT INTO Event (EPDate, ED, ENOfP, ENOfO)
+VALUES
+('31', 'Tuesday', '360', 'Jane Mary'),
+('45', 'Monday', '45', 'Aaron Doe');
+
