9012c6b8396b74c488b11ea1229b7209a5320c07
nyte-panda <mthuthimal@gmail.com>
2024-11-30 16:20:52 -0500

    Update queries.sql

--- SQL Files/queries.sql
+++ SQL Files/queries.sql
@@ -1,2 +1,5 @@
+-- Add your first query after this line, do not remove or edit this line
 SELECT SportsComplex.CL, Event.EPDate -- I'll finish this on Saturday --
 FROM 
+
+-- Add your second query after this line, do not remove or edit this line
