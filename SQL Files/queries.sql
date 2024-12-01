b19380f6bd9d1bd66d69b1729138f82c9cde947b
nyte-panda <mthuthimal@gmail.com>
2024-11-30 19:01:38 -0500

    Update queries.sql

--- SQL Files/queries.sql
+++ SQL Files/queries.sql
@@ -1,5 +1,8 @@
 -- Add your first query after this line, do not remove or edit this line
-SELECT SportsComplex.CL, Event.EPDate -- I'll finish this on Saturday --
-FROM 
+SELECT SportsComplex.CL, Event.EPDate
+FROM Holds
+WHERE Event.ED = 'BOXING'
+AND SportsComplex.SportsComplex_PK = Holds.SportsComplex_PK
+AND Event.Event_PK = Holds.Event_PK;
 
 -- Add your second query after this line, do not remove or edit this line
