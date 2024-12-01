6cc220afbcfa9384b10510b9e419200b9322ec8e
nyte-panda <mthuthimal@gmail.com>
2024-11-30 21:45:26 -0500

    Update queries.sql

--- SQL Files/queries.sql
+++ SQL Files/queries.sql
@@ -1,6 +1,6 @@
 -- Add your first query after this line, do not remove or edit this line
 SELECT SportsComplex.CL, Event.EPDate
 FROM SportsComplex, Event, Holds
-WHERE Event.ED = 'BOXING'
+WHERE Event.ED = 'BOXING' -- this is wrong -- 
 
 -- Add your second query after this line, do not remove or edit this line
