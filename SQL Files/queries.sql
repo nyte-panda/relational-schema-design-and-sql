59fcf8be2c694efa90296316b47acea7b2428598
Winnielepuh <114607910+Winnielepuh@users.noreply.github.com>
2024-12-01 00:43:59 -0500

    Update queries.sql

--- SQL Files/queries.sql
+++ SQL Files/queries.sql
@@ -1,6 +1,10 @@
 -- Add your first query after this line, do not remove or edit this line
 SELECT SportsComplex.CL, Event.EPDate
-FROM SportsComplex, Event, Holds
-WHERE Event.ED = 'BOXING' -- this is wrong -- 
+FROM Event, SportsComplex, Holds
+WHERE Event.ED LIKE '%Boxing%'
+  AND SportsComplex.SportsComplex_PK = Holds.SportsComplex_PK
+  AND Event.Event_PK = Holds.Event_PK
+ORDER BY Event.Event_PK, SportsComplex.SportsComplex_PK
+;
 
 -- Add your second query after this line, do not remove or edit this line
